f08207f5a2d43257a0993adb8730500c1ef9fef3
akshu1 <akshathanagaraj98@gmail.com>
2022-11-28 15:44:43 +0530

    Add:Basic Core java and Functional Programming Practice Problem

---
+++ src/test/java/com/bridgelabz/Day5/Assignment/Day5AssignmentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bridgelabz.Day5.Assignment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Day5AssignmentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
